1a2f9f5715bc1af254a37d38368830d757e2a82a
justinembawomye <jmbawomye@gmail.com>
2019-03-02 23:55:43 +0300

    add user class

---
+++ application/models.py
@@ -0,0 +1,32 @@
+from application import db
+
+class User(db.Model):
+
+    """This class represents the bucketlist table."""
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Posts', backref='author', lazy=True)
+
+
+    def __init__(self, username):
+        """initialize with name."""
+        self.username = name
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return User.query.all()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"User: {self.username}"
